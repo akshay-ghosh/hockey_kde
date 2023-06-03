8ef305cd3f7822bc8a6f26134c2dd7168ad81152
Akshay Ghosh <42668323+akshay-ghosh@users.noreply.github.com>
2023-06-03 16:28:40 -0300

    Add files via upload

---
+++ hockey_kde/src/hockey_variability/test_analysis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  3 16:24:52 2023
+
+@author: akshayghosh
+"""
+
+import hockey_variability as hv
+
+hv.calc_shot_kde(specific_game = True,OFF_team = 'OTT',date_string = '2023-03-23', DEF_team = None,playoffs = False)
